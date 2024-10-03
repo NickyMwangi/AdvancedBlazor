35cfd9d63cbf8c9ff1bf38db5f1630d660acb4f1
Nicky Lause <Niclausel@gmail.com>
2024-10-03 13:19:23 +0300

    Add project files.

---
+++ Web/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   = string.Empty;
+        public string Description { get; set; }  = string.Empty ;
+        public double Price { get; set; }     
+        public bool IsActive { get; set; }
+        public List<ProductProps> ProdProps { get; set; }
+    }
+
+    public class ProductProps
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; }  = string.Empty;
+
+    }
+}
