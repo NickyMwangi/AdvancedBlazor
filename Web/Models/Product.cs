d6936bfd1c8307b33399d4003b572b79f9396294
Nicky Lause <Niclausel@gmail.com>
2024-10-04 10:33:12 +0300

    lifecycle and forrms

--- Web/Models/Product.cs
+++ Web/Models/Product.cs
@@ -1,14 +1,23 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }   = string.Empty;
+        [Required]
         public string Description { get; set; }  = string.Empty ;
+        [Required]
+        [Range(1,10000000)]
         public double Price { get; set; }     
         public bool IsActive { get; set; }
+        [Required]
+        public DateTime IsAvailableFrom { get; set; }= DateTime.Now;
+        [Required]
+        public Categories Category { get; set; }
         public List<ProductProps> ProdProps { get; set; }
     }
 
@@ -18,5 +27,13 @@ namespace Web.Models
         public string Key { get; set; } = string.Empty;
         public string Value { get; set; }  = string.Empty;
 
+    }
+
+    public enum Categories
+    {
+        Fruits,
+        Desserts,
+        Appetizer,
+        Electronics
     }
 }
