a18168da5b4b31ce80a97a808d7bae9820cbfa8a
Nicky Lause <Niclausel@gmail.com>
2024-10-08 12:47:55 +0300

    using theme settings

---
+++ Web/wwwroot/js/plugins/custom.js
@@ -0,0 +1,118 @@
+﻿(function ($) {
+    "use strict";
+
+    // ______________ Page Loading
+    $(window).on("load", function (e) {
+        $("#global-loader").fadeOut("slow");
+    })
+
+
+
+    // ______________Active Class
+    $(document).ready(function () {
+        $(".horizontalMenu-list li a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().prev().click(); // click the item to make it drop
+            }
+        });
+        $(".horizontal-megamenu li a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().parent().parent().parent().parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().prev().click(); // click the item to make it drop
+            }
+        });
+        $(".horizontalMenu-list .sub-menu .sub-menu li a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().prev().click(); // click the item to make it drop
+            }
+        });
+    });
+
+    // ______________Back to top Button
+    $(window).on("scroll", function (e) {
+        if ($(this).scrollTop() > 0) {
+            $('#back-to-top').fadeIn('slow');
+        } else {
+            $('#back-to-top').fadeOut('slow');
+        }
+    });
+    $("#back-to-top").on("click", function (e) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 0);
+        return false;
+    });
+
+    // ______________ Global Search
+    $(document).on("click", "[data-bs-toggle='search']", function (e) {
+        var body = $("body");
+
+        if (body.hasClass('search-gone')) {
+            body.addClass('search-gone');
+            body.removeClass('search-show');
+        } else {
+            body.removeClass('search-gone');
+            body.addClass('search-show');
+        }
+    });
+    var toggleSidebar = function () {
+        var w = $(window);
+        if (w.outerWidth() <= 1024) {
+            $("body").addClass("sidebar-gone");
+            $(document).off("click", "body").on("click", "body", function (e) {
+                if ($(e.target).hasClass('sidebar-show') || $(e.target).hasClass('search-show')) {
+                    $("body").removeClass("sidebar-show");
+                    $("body").addClass("sidebar-gone");
+                    $("body").removeClass("search-show");
+                }
+            });
+        } else {
+            $("body").removeClass("sidebar-gone");
+        }
+    }
+    toggleSidebar();
+    $(window).resize(toggleSidebar);
+
+    $(document).on("click", ".close-btn", function () {
+        $("body").removeClass("search-show");
+    });
+
+    const DIV_CARD = 'div.card';
+
+    // ______________ Attach Remove
+    $(document).on('click', '[data-toggle="remove"]', function (e) {
+        let $a = $(this).closest(".attach-supportfiles");
+        $a.remove();
+        e.preventDefault();
+        return false;
+    });
+
+
+    // ______________ SWITCHER-toggle ______________//
+    $('.layout-setting').on("click", function (e) {
+        if (document) {
+            $('body').toggleClass('dark-mode');
+        } else {
+            $('body').removeClass('dark-mode');
+            $('body').addClass('light-mode');
+        }
+    });
+    $('.default-menu').on('click', function () {
+        var ww = document.body.clientWidth;
+        if (ww >= 992) {
+            $('body').removeClass('sidenav-toggled');
+        }
+    });
+
+})(jQuery);
